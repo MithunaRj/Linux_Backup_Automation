b965dc057862530d0615481d22e1a405acec7f20
Mithuna Raj <mithunarj123@gmail.com>
2026-05-30 13:48:30 +0000

    Implemented automated backup creation and reporting

---
+++ scripts/backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+output_file=reports/report.log
+timestamp=$(date +%Y_%m_%d-%H_%M_%S)
+echo "      " >> $output_file
+echo "---- New Backup Script ----" >> $output_file
+tar -czvf backups/backup_$timestamp.tar.gz source_data >> $output_file
+echo "         " >> $output_file
+if [ $? -eq  0 ]; then
+	echo "Backup Status: Successful" >> $output_file
+else
+	echo "Backup Status: Failed" >> $output_file
+fi
+echo "Generated Backup: backup_$timestamp" >> $output_file
+
